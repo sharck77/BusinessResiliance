eb25548232da881951b0f5a847aa3381f3cb042a
sharck77 <67459068+sharck77@users.noreply.github.com>
2025-04-07 16:23:56 +0300

    Update useOnlineStatus.js

--- hooks/useOnlineStatus.js
+++ hooks/useOnlineStatus.js
@@ -1,19 +1,15 @@
-import { useEffect, useState } from 'react';
+// hooks/useOnlineStatus.js
+import { useState, useEffect } from 'react';
+import NetInfo from '@react-native-community/netinfo';
 
 export function useOnlineStatus() {
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [isOnline, setIsOnline] = useState(true);
 
   useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
-
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsOnline(state.isConnected);
+    });
+    return () => unsubscribe();
   }, []);
 
   return isOnline;
